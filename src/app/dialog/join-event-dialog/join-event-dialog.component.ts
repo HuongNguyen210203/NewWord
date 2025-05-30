59c6115be0ab3b5202cea0614c21407abb3844dd
LaloTheDrug <kiet0966923001@gmail.com>
2025-05-30 20:07:24 +0700

    more-job

---
+++ src/app/dialog/join-event-dialog/join-event-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-event-dialog',
+  imports: [],
+  templateUrl: './join-event-dialog.component.html',
+  styleUrl: './join-event-dialog.component.css'
+})
+export class JoinEventDialogComponent {
+
+}
